f31f97c091f3263d9926f93c7e5370bb37bb5f95
SingleCorner <sinerwr@gmail.com>
2017-08-10 11:05:49 +0800

    fix_SICO-5

--- redis/main.go
+++ redis/main.go
@@ -10,6 +10,15 @@ package redis
 
 import (
 	"github.com/garyburd/redigo/redis"
+	"github.com/getsentry/raven-go"
+
+	"github.com/SiCo-Ops/cfg"
+)
+
+var (
+	config = cfg.Config
+
+	OpenAccessPool *redis.Pool
 )
 
 func RedisSetWithExpire(r *redis.Pool, key string, value interface{}, time int16) error {
@@ -33,7 +42,7 @@ func RedisGetWithKey(r *redis.Pool, key string) (interface{}, error, error) {
 	conn := r.Get()
 	err := conn.Err()
 	defer conn.Close()
-	data, err2 := conn.Do("GET", k)
+	data, err2 := conn.Do("GET", key)
 	return data, err, err2
 }
 
@@ -47,21 +56,26 @@ func RedisValueIsString(v interface{}) (string, error) {
 	return redis.String(v, err)
 }
 
-// func init() {
-// RedisPool = &redis.Pool{
-//  MaxIdle:   80,
-//  MaxActive: 12000, // max number of connections
-//  Dial: func() (redis.Conn, error) {
-//    c, err := redis.Dial("tcp", config.Redis.Te+":"+config.Redis.Default.Port)
-//    if err != nil {
-//      raven.CaptureError(err, nil)
-//    }
-//    return c, err
-//  },
-// }
+func init() {
+	OpenAccessPool = &redis.Pool{
+		MaxIdle:   80,
+		MaxActive: 2000,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", config.Redis.OpenAccess.Host+":"+config.Redis.OpenAccess.Port)
+			if err != nil {
+				raven.CaptureError(err, nil)
+				return c, err
+			}
+			if config.Redis.OpenAccess.Auth != "" {
+				_, err := c.Do("AUTH", config.Redis.OpenAccess.Auth)
+				if err != nil {
+					raven.CaptureError(err, nil)
+					c.Close()
+					return nil, err
+				}
+			}
+			return c, err
 
-// err := RedisPool.Get().Close()
-// if err != nil {
-//  raven.CaptureError(err, nil)
-// }
-// }
+		},
+	}
+}
